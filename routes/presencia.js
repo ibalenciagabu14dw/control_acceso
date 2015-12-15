8541fd980c93d3d25e36d88dbe03885fbe364e30
natiexperiencia <natiexperiencia@gmail.com>
2015-12-15 12:12:12 +0100

    presencia ok

--- routes/presencia.js
+++ routes/presencia.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.send(req.query.idT);
+  res.send(req.query.idT+"<br/>"+req.query.time+"<br/>"+req.query.room);
 });
 
 module.exports = router;

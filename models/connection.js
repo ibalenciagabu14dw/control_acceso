acd79650cfd32b5f4703688b9ada367309db8701
natiexperiencia <natiexperiencia@gmail.com>
2015-12-17 09:52:47 +0100

    connection ok

--- models/connection.js
+++ models/connection.js
@@ -2,10 +2,10 @@ var mysql = require('mysql'),
 
 connection = mysql.createConnection(
 	{ 
-		host: process.env.OPENSHIFT_MYSQL_DB_HOST || 'localhost',
+		host: process.env.OPENSHIFT_MYSQL_DB_HOST || 'localhost' || process.env.IP,
 		port :process.env.OPENSHIFT_MYSQL_DB_PORT || '3306',
-		user: process.env.OPENSHIFT_MYSQL_DB_USERNAME || 'root',
-		password: process.env.OPENSHIFT_MYSQL_DB_PASSWORD || 'zubiri',
+		user: process.env.OPENSHIFT_MYSQL_DB_USERNAME || 'root' || process.env.C9_USER,
+		password: process.env.OPENSHIFT_MYSQL_DB_PASSWORD || 'zubiri' || '',
 		database: 'controlfid'
 
 		

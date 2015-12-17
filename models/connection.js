cb8c43263e82a39c53d47ffb62163dd12613f48d
zubiri <zubiri@zubiri-VirtualBox.(none)>
2015-12-17 08:40:50 +0100

    mysql

--- models/connection.js
+++ models/connection.js
@@ -2,10 +2,16 @@ var mysql = require('mysql'),
 
 connection = mysql.createConnection(
 	{ 
-		host: 'localhost', 
-		user: 'root',  
-		password: 'zubiri', 
-		database: 'controlfid'
+		/*host: '127.2.85.2:3306', 
+		user: 'admin1wNN6rU',  
+		password: 'pK3_Nh5PQS2g', 
+		database: 'controlfid'*/
+
+		OPENSHIFT_MYSQL_DB_PORT='3306',
+		OPENSHIFT_MYSQL_DB_HOST='127.2.85.2',
+		OPENSHIFT_MYSQL_DB_PASSWORD='pK3_Nh5PQS2g',
+		OPENSHIFT_MYSQL_DB_USERNAME='admin1wNN6rU',
+		OPENSHIFT_MYSQL_DB_URL='mysql://admin1wNN6rU:pK3_Nh5PQS2g@127.2.85.2:3306/'
 	}
 );
 

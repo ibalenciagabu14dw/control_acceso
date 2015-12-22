8c2da0d875c93df3543e3e984aaccbce39aaad60
egoitz <egoitz>
2015-12-22 22:05:14 +0100

    vistaProfesor ok

--- routes/vistaProfesor.js
+++ routes/vistaProfesor.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var profesor = require('../models/profesor');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('vistaProfesor', { title: 'Vista Clase' });
+  profesor.losAlumnosDeSuClaseActual(req.query.idProfesor,req.query.time,function (error,data){
+									if (error) {
+										console.log("Fallo");
+										throw error;
+									}else{
+										console.log(data);
+										//res.send(data);
+										res.render("vistaProfesor",{ 
+										title : "vistaProfesor", 
+										//info: JSON.stringify(data)
+										info: data
+									});
+
+									}//else error
+								});
 });
 
 module.exports = router;

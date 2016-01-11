2239347743212a4a3323421046ec27271abfff0e
natiexperiencia <natiexperiencia@gmail.com>
2016-01-11 13:55:48 +0100

    horario profesor ok

--- public/js/vistaProfesor.js
+++ public/js/vistaProfesor.js
@@ -6,11 +6,26 @@ $(document).ready(function() {
     dataType: 'json',
     data: {correo: correo},
     success:function (data) {
-      var resp = "";
+      var cont = 1;
+      var resp = "<table id='tablaHorario' class='table table-hover tablesorter'><thead><tr><th>Día</th><th>Inicio</th><th>Fin</th><th>Aula</th><th>Asignatura</th><th>Grupo</th></tr></thead><tbody>";
       for (var i = 0; i < data.length; i++) {
-        resp+= data[i].dia_semana;
+        if (cont%2 == 0) {
+          resp += "<tr>";
+        }else{
+          resp += "<tr class='success'>";
+        }
+        resp += "<td>"+data[i].dia_semana+"</td>";
+        resp += "<td>"+data[i].hora_inicio+"</td>";
+        resp += "<td>"+data[i].hora_final+"</td>";
+        resp += "<td>"+data[i].numero+"</td>";
+        resp += "<td>"+data[i].nombre+"</td>";
+        resp += "<td>"+data[i].nombre_grupo+"</td>";
+        resp += "</tr>";
+        cont++;
       };
+      resp += "</tbody>";
       $('#horario').html(resp);
+      $('#horario #tablaHorario').tablesorter();
     }
   })
   .done(function() {
@@ -24,10 +39,9 @@ $(document).ready(function() {
 	$( "#horario").dialog({
       autoOpen: false,
       modal:true,
-      maxWidth:600,
-      maxHeight: 500,
-      width: 600,
-      height: 500,
+      maxWidth:700,
+      maxHeight: 700,
+      width: 700,
       show: {
         effect: "blind",
         duration: 1000
@@ -41,4 +55,6 @@ $(document).ready(function() {
 	$('#botonHorarioProfesor').click(function() {
 		$( "#horario" ).dialog( "open" );
     });
-});
+
+  
+});//ready

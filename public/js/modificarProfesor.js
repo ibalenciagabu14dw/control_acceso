22efcc504f2dde1698dcee72f562943b0811890b
egoitz barrena <egoitz barrena>
2016-01-12 14:02:44 +0100

    undefined campos input

--- public/js/modificarProfesor.js
+++ public/js/modificarProfesor.js
@@ -14,10 +14,23 @@ $(document).ready(function() {
 	//Crear formulario para modificar o borrar alumno al clicar en la celda
 	$('#resultado').on("click",".celda",function () {
 		var datos = $(this).contents();
+		console.log("------------------------");
+		console.log(datos[0]);
+		console.log("-------------------------");
 		buscarAlumnoPorId(datos[0].id)
 		.done(function(result) {
-    		var formulario = "<form class='form-group' id='formUpdate'>Id: <input type='text' id='id' name='id' class='form-control' value='"+result.id+"' disabled='disabled'>";
+    		var formulario = "<form class='form-group' id='formUpdate'>id_profesor: <input type='text' id='id_profesor' name='id_profesor' class='form-control' value='"+result.id_profesor+"' disabled='disabled'>";
+    		formulario += "dni: <input type='text' id='dni' name='dni' class='form-control' value='"+result.dni+"'>";
     		formulario += "Nombre: <input type='text' id='nombre' name='nombre' class='form-control' value='"+result.nombre+"'>";
+    		formulario += "Apellidos: <input type='text' id='apellidos' name='apellidos' class='form-control' value='"+result.apellidos+"'>";
+    		formulario += "Correo: <input type='text' id='correo' name='correo' class='form-control' value='"+result.correo+"'>";
+    		formulario += "Password: <input type='text' id='password' name='password' class='form-control' value='"+result.password+"'>";
+    		formulario += "Foto: <input type='file' id='foto' name='foto' class='form-control' value='"+result.nombre+"'>";
+    		formulario += "Tarj_act: <input type='text' id='tarjeta_activada' name='tarjeta_activada' class='form-control' value='"+result.tarjeta_activada+"'>";
+    		formulario += "Numero_Tarjeta: <input type='number' id='num_tarjeta' name='num_tarjeta' class='form-control' value='"+result.num_tarjeta+"'>";
+    		formulario += "Admin: <input type='text' id='admin' name='admin' class='form-control' value='"+result.admin+"'>";
+    		//formulario += "Tipo: <input type='text' id='nombre' name='nombre' class='form-control' value='"+result.nombre+"'>";
+    		//formulario += "Asignaturas: <input type='text' id='nombre' name='nombre' class='form-control' value='"+result.nombre+"'>";
     		formulario += "<br/><input type='submit' id='btnModificar' class='btn btn-warning' value='Modificar'>";
     		formulario += "&nbsp;<button id='btnBorrar' class='btn btn-danger'>Borrar</button>";
     		formulario += "&nbsp;<button id='btnVolver' class='btn btn-primary'>Volver</button>";
@@ -57,8 +70,8 @@ $(document).ready(function() {
 	}//function buscarAlumnos
 
 	//funcion para buscar alumnos por id
-	function buscarAlumnoPorId (id_profesor) {
-		var dato = { 'id_profesor':id_profesor };
+	function buscarAlumnoPorId (id) {
+		var dato = { id_profesor:id };
 		return	$.ajax({
 					url: 'buscarProfesorId',
 					type: 'post',

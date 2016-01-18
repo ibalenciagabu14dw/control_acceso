0bc6da4e85d32dbec79ab6fed510d0a0b447103b
ibalenciagabu14dw <ibalenciagabu14dw@ikzubirimanteo.com>
2016-01-18 10:09:07 +0000

    agregarGrupo

--- routes/configFuncionamiento.js
+++ routes/configFuncionamiento.js
@@ -241,8 +241,8 @@ router.post('/agregarAula', function(req,res){
 /* POST agregar grupo page. */
 router.post('/agregarGrupo', function(req,res){
   console.log(req.body);
-  var nombre = req.body.numero;
-  var tipo = req.body.piso;
+  var nombre = req.body.nombre;
+  var tipo = req.body.tipo;
   grupo.insertarGrupo(nombre,tipo, function (error) {
     if (error) {
       throw error;

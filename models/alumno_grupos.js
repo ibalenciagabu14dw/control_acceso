90710fb6768901a6025489a182f952a74d4188f1
ibalenciaga <balenciaga@hotmail.com>
2016-01-26 18:56:23 +0100

    ordenar modelo alumno_grupos OK

--- models/alumno_grupos.js
+++ models/alumno_grupos.js
@@ -3,12 +3,17 @@ var app = require('../app');
 
 var alumno_grupos = {};
 
+/***********************************************************INSERT*********************************************************/
+
+/*
+* INSERTAR alumno en un grupo
+*/
 alumno_grupos.agregarAlumnoGrupo =  function(id_grupo,id_alumno,callback) {
 	if(connection){
 		var alumno_grupos = { id_grupo: id_grupo, id_alumno: id_alumno};						
 		var sqlagregarAlumnoGrupo = 'INSERT INTO alumno_grupos SET ?';
 		connection.query(sqlagregarAlumnoGrupo,alumno_grupos, function(error){
-		  if (error) {
+		  	if (error) {
 				throw error;
 				console.log(error);
 			}else{
@@ -18,6 +23,13 @@ alumno_grupos.agregarAlumnoGrupo =  function(id_grupo,id_alumno,callback) {
 	}//if
 }//alumno_grupos.agregarAlumnoGrupo
 
+/****************************************************************************************************************************/
+
+/***********************************************************DELETE***********************************************************/
+
+/*
+* DELETE alumno de un grupo
+*/
 alumno_grupos.borrarAlumnoGrupos =  function(id_alumno,callback) {
 	if(connection){					
 		var sqlborrarAlumnoGrupos = 'DELETE FROM alumno_grupos WHERE id_alumno= "'+id_alumno+'"';
@@ -25,10 +37,12 @@ alumno_grupos.borrarAlumnoGrupos =  function(id_alumno,callback) {
 		  if (error) {
 				throw error;
 			}else{
-				//console.log('borrarAlumnoGrupos correctamente');
-			}//.else
-		});//.connection.query
-	}//.if (connection)
+				console.log('borrarAlumnoGrupos OK');
+			}//else
+		});//connection.query
+	}//if
 }//alumno_grupos.borrarAlumnoGrupos
+
+/***********************************************************DELETE***********************************************************/
 
 module.exports = alumno_grupos;

ac1784f4956bbcf1a4527dc16beb674d4612dd74
ibalenciaga <balenciaga@hotmail.com>
2016-01-26 12:10:26 +0100

    separando configFuncionamiento

--- routes/configGrupo.js
+++ routes/configGrupo.js
@@ -146,15 +146,14 @@ router.post('/borrarGrupo', function(req,res,next){
   })//borrarGrupo
 });//router.post('/borrarGrupo', function(req,res,next){  
 
-router.post('/mostrarTodosLosGruposIdNombre', function(req,res,next){//FALTA EDITAR
+router.post('/mostrarTodosLosGruposIdNombre', function(req,res,next){
   grupo.mostrarTodosLosIdNombreGrupo(function(error,row) {
     if (error) {
       throw error;
     }else{
-      console.log(row);
       res.send(row);
     }
-  })//borrarGrupo
-});//router.post('/borrarGrupo', function(req,res,next){  
+  })//.grupo.mostrarTodosLosIdNombreGrupo
+});//.router.post('/mostrarTodosLosGruposIdNombre', function(req,res,next){
 
 module.exports = router;

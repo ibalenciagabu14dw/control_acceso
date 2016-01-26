95b7153687feefb6fd82f6614314a2715771f754
ibalenciaga <balenciaga@hotmail.com>
2016-01-26 08:47:43 +0100

    routes config creados

---
+++ routes/api.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var router = express.Router();
+var time = require('../models/time');
+var alumno = require('../models/alumno');
+var asignatura = require('../models/asignatura');
+
+/*
+******************************MOSTAR********************************
+*/
+
+router.get('/mostrar/mostrarAlumnoportarjeta', function(req, res, next) {
+	alumno.buscarAlumnoPorTarjeta(req.query.idT, function (error,data) {
+		if (error) {
+			res.send("ko");
+			console.log("Fallo update presencia alumno");
+			throw error;
+		}else{
+			//emitir al cliente para cambiar color presencia alumno
+			res.json(data);
+		}//else error
+	});//UpdatePresenciaAlumno
+});//
+
+router.get('/mostrar/buscarTodoslosAlumnos', function(req, res, next) {
+	alumno.buscarTodoslosAlumnos(function (error,data) {
+		if (error) {
+			res.send("ko");
+			console.log("Fallo update presencia alumno");
+			throw error;
+		}else{
+			//emitir al cliente para cambiar color presencia alumno
+			res.json(data);
+		}//else error
+	});//UpdatePresenciaAlumno
+});//
+
+/*
+******************************AGREGAR********************************
+*/
+
+router.post('/agregar/insertarAsigntura', function(req, res, next) {
+	asignatura.insertarAsigntura(req.query.nombre,req.query.clave,req.query.obligatoria,req.query.tipo,function (error,data) {
+		if (error) {
+			res.send("ko");
+			console.log("Fallo agregarAlumno");
+			throw error;
+		}else{
+			//emitir al cliente para cambiar color presencia alumno
+			res.json(data);
+		}//else error
+	});//UpdatePresenciaAlumno
+});//
+
+
+router.post('/agregar/insertarSinFoto', function(req, res, next) {
+	alumno.agregarAlumno(req.query.dni,req.query.nombre,req.query.apellidos,req.query.correo,req.query.num_tarjeta,function (error,data) {
+		if (error) {
+			res.send("ko");
+			console.log("Fallo insertarSinFoto");
+			throw error;
+		}else{
+			//emitir al cliente para cambiar color presencia alumno
+			res.json(data);
+		}//else error
+	});//insertarSinFoto
+});//
+module.exports = router;
---
+++ routes/configAula.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var router = express.Router();
+var aula = require('../models/aula');
+var multer = require('multer');
+
+module.exports = router;

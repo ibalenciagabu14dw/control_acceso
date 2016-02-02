28c2a14a169f147e09c6dfa2bec20e42c1f9632d
ibalenciaga <balenciaga@hotmail.com>
2016-02-02 17:36:00 +0100

    creando api

--- routes/api.js
+++ routes/api.js
@@ -56,137 +56,97 @@ router.post('/agregarAlumno', function(req, res, next) {
     });//alumno.buscarAlumnoPorDni
 });//router.post('/agregarAlumno
 
+/*
+* INSERT asignatura ****NO FUNCIONA****
+*/
+router.post('/agregarAsignatura', function(req,res,next){
+    asignatura.buscarAsignaturaPorClave(req.body.clave, function (error,row) {
+        if (error) {
+            res.send({err:'bd'});
+            throw error;
+        }else{
+            if(row.length>0){
+                res.send({err:'existe'});
+            }else{
+                asignatura.agregarAsignatura(req.body.nombre,req.body.clave,req.body.obligatoria,req.body.tipo, function (error,row) {
+                    if (error) {
+                        res.send({err:'bd'});
+                        throw error;
+                    }else{
+                        res.send(row);
+                    }//else
+                });//asignatura.agregarAsignatura
+            }//else
+        }//else
+    });//asignatura.buscarAsignaturaPorClave
+});//router.post('/agregarAsignatura
+
 /****************************************************************************************************************************/
 
 /***********************************************************UPDATE***********************************************************/
 
 /*
-* UPDATE alumno sin foto
+* UPDATE alumno 
 */
 router.post('/modificarAlumno', function(req, res, next) {
-	var id_alumno = req.body.id_alumno;
-    var dni = req.body.dni;
-    var nombre = req.body.nombre;
-    var apellidos = req.body.apellidos;
-    var correo = req.body.correo;
-    var tarjeta_activada = req.body.tarjeta_activada;
-    var num_tarjeta = req.body.num_tarjeta;
-    
-	alumno_grupos.borrarAlumnoGrupos(req.body.id_alumno, function(error,row) {
+
+});//router.post('/modificarAlumno
+
+/****************************************************************************************************************************/
+
+/***********************************************************DELETE***********************************************************/
+
+/*
+* DELETE alumno por id_alumno ****NO FUNCIONA****
+*/
+router.post('/borrarAlumno', function(req,res,next){
+    alumno.borrarAlumno(req.body.id_alumno, function(error,row) {
         if (error) {
             throw error;
         }else{
             res.send(row);
-        }
-    })//alumno_grupos.borrarAlumnoGrupos
-  
-    var data= req.body.grupo;
-    for (var i = 0; i < data.length; i++) {
-        alumno_grupos.agregarAlumnoGrupo(data[i],req.body.id_alumno, function(error,row) {
-            if (error) {
-                throw error;
-            }else{
-                res.send(row);
-            }//else
-        })//alumno_grupos.agregarAlumnoGrupo
-    }//for
+        }//else
+    })//alumno.borrarAlumno
+});//router.post('/borrarAlumno
 
-    if(req.body.asignatura == undefined){
-        //console.log("el alumno no tiene ninguna convalidada");
-    }else {
-        convalidadas.borrarAsignaturaConvalidada(req.body.id_alumno, function(error,row) {
-            if (error) {
-                throw error;
-            }else{
-                res.send(row);
-            }//else
-        })//convalidadas.borrarAsignaturaConvalidada
-        
-        var data2= req.body.asignatura;
-        for (var i = 0; i < data2.length; i++) {
-            convalidadas.agregarAsignaturaConvalidada(data2[i],req.body.id_alumno, function(error,row) {
-                if (error) {
-                    throw error;
-                }else{
-                    res.send(row);
-                }//else
-            })//convalidadas.agregarAsignaturaConvalidada
-        }//for
-    }//else
 
-    
-    alumno.buscarAlumnoPorIdDniCorreoNum_tarj(id_alumno,dni,correo,num_tarjeta, function(error,row) {
+/****************************************************************************************************************************/
+
+/***********************************************************SELECT***********************************************************/
+
+/*
+* BUSCAR alumno por nombre ****NO FUNCIONA****ERROR DE FOTO
+*/
+router.post('/buscarAlumnoPorNombre', function(req,res,next) {
+    alumno.buscarAlumnoPorNombre(req.body.nombre, function(error,row) {
         if (error) {
             throw error;
         }else{
-            if(row.length>0){
-                alumno.modificarAlumnoSinFoto(id_alumno,dni,nombre,apellidos,correo,num_tarjeta,tarjeta_activada, function(error,row) {
-                    if (error) {
-                        throw error;          
-                    }else {
-                        res.send(row);    
-                    }        
-                })//.alumno.modificarAlumnoSinFoto
-            } else {
-                alumno.buscarAlumnoPorDni(dni, function(error,row) {
-                    if (error) {
-                        throw error;
-                    } else {
-                        if(row.length>0){
-                            res.send({err:'existeDNI'});
-                        } else {
-                            alumno.buscarAlumnoPorCorreo(correo, function(error,row){
-                                if (error) {
-                                    throw error;
-                                } else {
-                                    if(row.length>0){
-                                        res.send({err:'existeCorreo'});
-                                    } else {
-                                        alumno.buscarAlumnoPorTarjeta(num_tarjeta, function(error,row){
-                                            if (error) {
-                                               throw error; 
-                                           } else {
-                                            if(row.length>0){
-                                                res.send({err:'existeTarjeta'});
-                                            } else {
-                                                alumno.modificarAlumnoSinFoto(id_alumno,dni,nombre,apellidos,correo,num_tarjeta,tarjeta_activada, function(error,row){
-                                                    if (error) {
-                                                        throw error;
-                                                    } else {
-                                                        res.send(row); 
-                                                    }//.else if (error) 
-                                                })//.alumno.modificarAlumnoSinFoto
-                                            }//else if(row.length>0){
-                                           }//.else if (error)
-                                        })//.alumno.buscarAlumnoPorTarjeta
-                                    }//.else if(row.length>0)
-                                }//.else if (error)
-                            })//.alumno.buscarAlumnoPorCorreo       
-                        }//.else if(row.length>0)
-                    }//.else if (error)
-                })//.alumno.buscarAlumnoPorDni
-            }//.else if(row.length<0)
-        }//.else if (error)
-    })//.alumno.buscarAlumnoPorIdDniCorreoNum_tarj
-});//modificarAlumno
+            res.send(row);
+        }//else
+    })//alumno.buscarAlumnoPorNombre
+});//router.post('/buscarAlumnoPorNombre
+
+/*
+* BUSCAR alumno por id_alumno ****NO FUNCIONA****ERROR DE FOTO
+*/
+router.post('/buscarAlumnoPorId', function(req,res,next) {
+    alumno.buscarAlumnoPorId(req.body.id_alumno, function(error,row) {
+        if (error) {
+            throw error;
+        }else{
+            res.json(row);
+        }//else
+    })//alumno.buscarAlumnoPorId
+});//router.post('/buscarAlumnoPorId
+
+
+/****************************************************************************************************************************/
 
 
 /*
 ******************************MOSTAR********************************
 
-
-router.get('/mostrar/mostrarAlumnoportarjeta', function(req, res, next) {
-	alumno.buscarAlumnoPorTarjeta(req.query.num_tarjeta, function (error,data) {
-		if (error) {
-			res.send("ko");
-			console.log("Fallo update presencia alumno");
-			throw error;
-		}else{
-			//emitir al cliente para cambiar color presencia alumno
-			res.json(data);
-		}//else error
-	});//modificarPresenciaDelAlumno
-});//
 
 router.get('/mostrar/buscarTodoslosAlumnos', function(req, res, next) {
 	alumno.buscarTodoslosAlumnos(function (error,data) {
@@ -203,22 +163,6 @@ router.get('/mostrar/buscarTodoslosAlumnos', function(req, res, next) {
 
 
 ******************************AGREGAR********************************
-
-
-router.post('/agregar/agregarAsignatura', function(req, res, next) {
-	asignatura.agregarAsignatura(req.query.nombre,req.query.clave,req.query.obligatoria,req.query.tipo,function (error,data) {
-		if (error) {
-			res.send("ko");
-			console.log("Fallo agregarAlumno");
-			throw error;
-		}else{
-			//emitir al cliente para cambiar color presencia alumno
-			res.json(data);
-		}//else error
-	});//modificarPresenciaDelAlumno
-});//
-
-
-//*/
+*/
 
 module.exports = router;

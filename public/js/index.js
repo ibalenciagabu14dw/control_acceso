3bd4a40ac2142c154a42a76365b1d4e344618d60
natiexperiencia <natiexperiencia@gmail.com>
2016-02-18 10:36:41 +0100

    arreglo footer index

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
+	//regla correo
+	jQuery.validator.addMethod("correo", function(value, element) {
+		return this.optional(element) || /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i.test(value);
+	});
 	var reglas = {
-		user:{required:true},
+		user:{required:true,correo:true},
 		pass:{required:true}
 	}
 	var mensajes = {
-		user:{required:" Requerido"},
+		user:{required:" Requerido",correo:" Correo no válido"},
 		pass:{required:" Requerido"}
 	}
 
@@ -12,4 +16,7 @@ $(document).ready(function() {
 		rules:reglas,
 		messages:mensajes,
 	})
+	if($('#info').html()!=undefined){
+		$('#footer').css('bottom', 'auto');
+	}
 });//ready
